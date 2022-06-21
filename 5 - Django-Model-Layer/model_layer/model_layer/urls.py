e1aa622da7a3d3012fa3b5cb2689c2164082ab4f
Víctor Gómez <vgomez@applaudostudios.dev>
2022-06-21 19:08:05 -0400

    Study ModelLayer Querysets

--- 5 - Django-Model-Layer/model_layer/model_layer/urls.py
+++ 5 - Django-Model-Layer/model_layer/model_layer/urls.py
@@ -1,4 +1,4 @@
-"""site_orm URL Configuration
+"""model_layer URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
